07dbefc4ba38dc742e529846c1800066db56694f
DevJoaoPedroGiancoli <joao.giancoli@usp.br>
2023-10-05 16:21:52 -0300

    first commit

---
+++ run_me.py
@@ -0,0 +1,17 @@
+from Detector import *
+
+modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
+
+classFile = 'coco.names'
+
+imagePath = "test/1.jpg"
+
+detector = Detector()
+
+detector.readClasses(classFile)
+
+detector.downloadModel(modelURL)
+
+detector.loadModel()
+
+detector.predictImage(imagePath)
